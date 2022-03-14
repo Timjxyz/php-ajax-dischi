41e70e676fb1a5b8158b8d7078e5be601125bae1
Timothy Gaither <timjgaither@gmail.com>
2022-03-14 16:18:03 +0100

    push

--- index_php_version.php
+++ index_php_version.php
@@ -10,22 +10,28 @@ require __DIR__ . '/database.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="./style.css">
- 
+    <link rel="stylesheet" href="./style_php.css">
+    
     <title>Document</title>
 </head>
 <body>
     <main class="container">
-        <?php
-           
-            foreach($database as $singleData){     
-                echo '<div class="box">';        
-                    echo "<h2>". $singleData['title']."</h2>";
-                    echo "<img src=".$singleData['poster']." />";              
-                echo"</div>";
-            }
+        <div class="row">
+            <?php
             
-       ?>
+                foreach($database as $singleData){     
+                    echo '<div class="box">';        
+                        echo "<img src=".$singleData['poster']." />";          
+                        echo "<h2>". $singleData['title']."</h2>";    
+                        echo "<div>". $singleData['author']."</div>";    
+                        echo "<div>". $singleData['genre']."</div>";   
+                        echo "<div>". $singleData['year']."</div>"; 
+                    echo"</div>";
+                }
+                
+            ?>
+
+        </div>
 
     </main>
 </body>
